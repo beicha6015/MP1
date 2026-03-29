6c14654bae575a6937b643576e6872b7ae5d10b8
LX <2236448769@qq.com>
2026-03-29 16:38:01 +0800

    update name and headline

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'LX '
+export const headline ='This is my web.'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "Im trying to do."
+export const email = 'aiden_6015@163.com'
+export const githubUsername = 'beicha6015'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
